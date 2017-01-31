c0984c6a85e82ef228413358226172621615f8d1
Alex Abbott <alex.abbott@bynd.com>
2017-01-31 12:26:57 -0500

    Add basic header styling

---
+++ themes/contrib/emulsify/components/_patterns/03-organisms/site/site-header/site-header.js
@@ -0,0 +1,41 @@
+// UNCOMMENT IF DRUPAL
+// Drupal.behaviors.header = {
+//   attach: function (context, settings) {
+
+// REMOVE IF DRUPAL
+(function() {
+
+    'use strict';
+
+    // Set 'document' to 'context' if Drupal
+    var menuToggle = document.querySelectorAll('.mobile-nav__icon');
+
+    // Mobile Menu Toggle
+    // Mobile Click Menu Transition
+    menuToggle[0].addEventListener('click', function (e) {
+    var className = 'is-active';
+
+    // Add is-active class
+    if (this.classList) {
+        this.classList.toggle(className);
+    } else {
+        var classes = this.className.split(' ');
+        var existingIndex = classes.indexOf(className);
+
+        if (existingIndex >= 0) {
+            classes.splice(existingIndex, 1);
+        }
+        else {
+            classes.push(className);
+        }
+        this.className = classes.join(' ');
+    }
+    e.preventDefault();
+    });
+
+// REMOVE IF DRUPAL
+})();
+
+// UNCOMMENT IF DRUPAL
+//   }
+// };
